26943200846b362f8e45fa317e16df9728b5d98c
pBinarySoul <pBinarySoul@gmail.com>
2021-10-14 19:00:37 +0300

    first commit

--- src/MainLoggedIn.js
+++ src/MainLoggedIn.js
@@ -1,16 +1,30 @@
 import React from "react";
-
+import {
+    
+    Link
+  } from "react-router-dom";
 
 function MainLoggedIn({setIsLoggedIn, loggedInUser}){
 
 
     return(
         <>
-            {loggedInUser}
-            <div onClick={()=>{localStorage.removeItem("loggedInUser");setIsLoggedIn(false);}}>Log out</div>
-
-
             
+            
+
+            <header className="header">
+        <div className="header-logo">
+          logo
+        </div>
+        <div className="header-buttons">
+          <Link to="/contacts"><button className="contacts-btn">Контакты</button></Link>
+          <button className="sign-btn" onClick={()=>{localStorage.removeItem("loggedInUser");setIsLoggedIn(false);}}>Выйти</button>
+        </div>
+      </header>
+      <div className="header-line"></div>
+
+        Привет, {loggedInUser}
+
         </>
     )
 
