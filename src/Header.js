d33a1ecd990d241dbfc47b44673c61935fe56010
topheroes <44710213+topheroes@users.noreply.github.com>
2021-10-16 12:18:29 +0300

    Add files via upload

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
 import React, {useContext} from "react";
 
 import {
-    
+  useHistory,
     Link
   } from "react-router-dom";
   import { isLoggedInContext } from "./contexts";
@@ -10,7 +10,7 @@ import {
 function Header({setIsOpen}){
     
     const [isLoggedIn, setIsLoggedIn] = useContext(isLoggedInContext);
-    
+    const history = useHistory();
     
 
     return(
@@ -21,7 +21,7 @@ function Header({setIsOpen}){
         </div>
         <div className="header-buttons">
           <Link to="/contacts"><button className="contacts-btn">Контакты</button></Link>
-          <button className="sign-btn" onClick={()=> isLoggedIn ?  (()=>{localStorage.removeItem("loggedInUser") ; setIsLoggedIn(false)})() : setIsOpen(true)}>{isLoggedIn ? "Выйти" : "Войти"}</button>
+          <button className="sign-btn" onClick={()=> isLoggedIn ?  (()=>{localStorage.removeItem("loggedInUser") ; setIsLoggedIn(false); history.push("/"); })() : setIsOpen(true)}>{isLoggedIn ? "Выйти" : "Войти"}</button>
           
         </div>
       </header>
